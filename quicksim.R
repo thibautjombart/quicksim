d03e50a07e6e5867fa7118b63daee63032b8ac3d
Thibaut Jombart <thibautjombart@gmail.com>
2018-04-05 22:14:01 +0100

    bulk of working code, quite tidy

--- quicksim.R
+++ quicksim.R
@@ -3,6 +3,36 @@
 ## sequences, etc.
 
 
+## INTERNAL FUNCTIONS
+
+modify_defaults <- function(defaults, x, strict = TRUE) {
+    extra <- setdiff(names(x), names(defaults))
+    if (strict && (length(extra) > 0L)) {
+        stop("Additional invalid options: ", paste(extra, collapse=", "))
+    }
+    utils::modifyList(defaults, x, keep.null = TRUE) # keep.null is needed here
+}
+
+
+new_config <- function(...) {
+    defaults <- list(x_min = 0, # min x, spatial coords
+                     x_max = 100, # max x, spatial coords
+                     y_min = 0, # min y, spatial coords
+                     y_max = 100, # max y, spatial coords
+                     sd_spatial = 1, # sd of normal spatial kernel
+                     genome_length = 3e4, # genome length
+                     mutation_rate = 1e-5, # mutation rate
+                     separation_lineages = 365 # nb days to ancestral lineage
+                     )
+    
+    args <- list(...)
+    
+    modify_defaults(defaults, args)    
+}
+
+
+
+
 
 
 ## SPATIAL COORDINATES
@@ -12,8 +42,13 @@
 
 ## x: infector's location
 
-new_location <- function(x, sd = 1) {
-    out <- rnorm(length(x), mean = x, sd = sd)
+new_location <- function(x = NULL, ..., config = new_config(...)) {
+    if (is.null(x)) {
+        out <- c(runif(1, min = config$x_min, max = config$x_max),
+                 runif(1, min = config$y_min, max = config$y_max))
+    } else {
+        out <- rnorm(length(x), mean = x, sd = config$sd_spatial)
+    }
     return(out)
 }
 
@@ -41,20 +76,36 @@ points(new_locs_sd5, col = "red")
 ## generation_time: the number of days between the infection of x and the new
 ## case
 
-## mu: mutation rate (per genome and per day)
+## mu: mutation rate (per site and per day)
 
 ## genome_length: total length of the genome
 
-new_dna <- function(x, generation_time, mu, genome_length) {
-    ## generate new mutations
-    n_mutations <- rpois(1, mu * generation_time)
-    new_mutations <- runif(n_mutations, min = 1, max = genome_length)
-    new_mutations <- as.integer(round(new_mutations))
-    out <- c(x, new_mutations)
+new_dna <- function(x = NULL, generation_time = NULL, ..., config = new_config(...)) {
 
-    ## cleanup: remove reverse mutations
-    reverse_mutations <- as.integer(names(which(table(out) %% 2 == 0L)))
-    out <- setdiff(out, reverse_mutations)
+    ## generate new lineage
+    
+    if (is.null(x)) {
+        out <- new_dna(integer(0),
+                     generation_time = config$separation_lineages,
+                     config = config)
+    } else {
+
+        if (is.null(generation_time)) {
+            stop("generation_time must be specified (currently NULL)")
+        }
+        
+        ## generate new mutations
+        lambda <- generation_time * config$mutation_rate * config$genome_length
+        n_mutations <- rpois(1, lambda)
+        new_mutations <- runif(n_mutations, min = 1, max = config$genome_length)
+        new_mutations <- as.integer(round(new_mutations))
+        out <- c(x, new_mutations)
+
+        ## cleanup: remove reverse mutations
+        reverse_mutations <- as.integer(names(which(table(out) %% 2 == 0L)))
+        out <- setdiff(out, reverse_mutations)
+    }
+    
     return(out)
 }
 
@@ -125,39 +176,16 @@ dist_dna(list(integer(0), integer(0), 1, 1, 1:2, 1:3, 1:4, 11:20))
 
 
 
-## INTERNAL FUNCTIONS
-
-modify_defaults <- function(defaults, x, strict = TRUE) {
-    extra <- setdiff(names(x), names(defaults))
-    if (strict && (length(extra) > 0L)) {
-        stop("Additional invalid options: ", paste(extra, collapse=", "))
-    }
-    utils::modifyList(defaults, x, keep.null = TRUE) # keep.null is needed here
-}
-
-
-default_config <- function(...) {
-    defaults <- list(x_min = 0, x_max = 0, y_min = 100, y_max = 100,
-                     genome_length = 3e4, mutation_rate = 5e-50)
-    
-    args <- list(...)
-    
-    modify_defaults(defaults, args)    
-}
-
-
-
-
 
 
 ## MAKE NEW INDIVIDUALS, EITHER FROM AN INFECTOR, OR FROM SCRATCH
 
-new_case <- function(infector = NULL) {
+new_case <- function(infector = NULL, ..., config = new_config(...)) {
     out <- list()
     
-    if (is.null(infector)) {
-        out$location <- location_default()
-    }
+    out$location <- new_location(infector, config = config)
 
-    
+    out$dna <- new_dna(infector, config = config)
+
+    return(out)
 }
