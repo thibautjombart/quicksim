56af20fe8a4898bdbfeb861a97d66512d40cbd1e
Thibaut Jombart <thibautjombart@gmail.com>
2018-04-05 21:46:45 +0100

    adding defaults handling

--- quicksim.R
+++ quicksim.R
@@ -1,5 +1,3 @@
-
-
 ## This script contains a series of functions which determine the state of a
 ## newly infected case given its infector - e.g. geographic location, genetic
 ## sequences, etc.
@@ -79,8 +77,8 @@ new_dna(1:10, 10, 1, 10) # reverse mutations delete old ones
 ## comparisons from a list of 'DNA sequences' (i.e. list of vectors of
 ## mutations).
 
-## x: a 'DNA sequence', stored as a vector of integers indicating mutations 
-## y: a 'DNA sequence', stored as a vector of integers indicating mutations 
+## x: a 'DNA sequence', stored as a vector of integers indicating mutations
+## y: a 'DNA sequence', stored as a vector of integers indicating mutations
 
 dist_dna_pair <- function(x, y) {
     common_mutations <- intersect(x, y)
@@ -109,17 +107,57 @@ dist_dna <- function(x) {
     n_pairs <- ncol(all_pairs)
     out <- dist(seq_len(N))
     out[] <- NA_integer_
-    
+
     ## browser()
     for (i in seq_along(all_pairs)) {
-        out[i] <- dist_dna_pair(x[[all_pairs[[i]][1]]],
-                                x[[all_pairs[[i]][2]]])
+	out[i] <- dist_dna_pair(x[[all_pairs[[i]][1]]],
+				x[[all_pairs[[i]][2]]])
     }
 
-    return(out)    
+    return(out)
 }
 
 
 ## examples
 dist_dna(list(integer(0), integer(0), 1, 1))
 dist_dna(list(integer(0), integer(0), 1, 1, 1:2, 1:3, 1:4, 11:20))
+
+
+
+
+## INTERNAL FUNCTIONS
+
+modify_defaults <- function(defaults, x, strict = TRUE) {
+    extra <- setdiff(names(x), names(defaults))
+    if (strict && (length(extra) > 0L)) {
+        stop("Additional invalid options: ", paste(extra, collapse=", "))
+    }
+    utils::modifyList(defaults, x, keep.null = TRUE) # keep.null is needed here
+}
+
+
+default_config <- function(...) {
+    defaults <- list(x_min = 0, x_max = 0, y_min = 100, y_max = 100,
+                     genome_length = 3e4, mutation_rate = 5e-50)
+    
+    args <- list(...)
+    
+    modify_defaults(defaults, args)    
+}
+
+
+
+
+
+
+## MAKE NEW INDIVIDUALS, EITHER FROM AN INFECTOR, OR FROM SCRATCH
+
+new_case <- function(infector = NULL) {
+    out <- list()
+    
+    if (is.null(infector)) {
+        out$location <- location_default()
+    }
+
+    
+}
