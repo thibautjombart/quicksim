46410eaf79f149c5a51a0f8c670b057584c51672
Thibaut Jombart <thibautjombart@gmail.com>
2018-04-05 22:38:27 +0100

    work in progress

--- R/new_location.R
+++ R/new_location.R
@@ -1,27 +1,39 @@
-## SPATIAL COORDINATES
+#' Create the location of a new case
+#'
+#' This function creates the location of a new case, with or without a known
+#' infector. If it is missing (NULL, default), the case is imported according to
+#' parameters in \code{config}.
+#'
+#' @export
+#'
+#' @param location An optional location of the infector; if provided, it must be
+#'     a list with the following slots: 'location', 'dna'
+#'
+#' @param ... further arguments passed to \code{new_config}
+#'
+#' @details This function uses a Normal spatial kernel for all spatial
+#'     dimensions, with a single parameter 'sd_spatial'.
 
-## This function uses a Normal spatial kernel for all spatial dimensions, with a
-## single parameter 'sd'.
 
-## x: infector's location
-
-new_location <- function(x = NULL, ..., config = new_config(...)) {
+new_location <- function(location = NULL, ..., config = new_config(...)) {
     if (is.null(x)) {
         out <- c(runif(1, min = config$x_min, max = config$x_max),
                  runif(1, min = config$y_min, max = config$y_max))
     } else {
-        out <- rnorm(length(x), mean = x, sd = config$sd_spatial)
+        out <- rnorm(length(location),
+                     mean = location,
+                     sd = config$sd_spatial)
     }
     return(out)
 }
 
 
-## example
+## ## example
 
-x_loc <- c(3, 5)
-new_locs_sd1 <- t(replicate(100, new_location(x_loc)))
-new_locs_sd5 <- t(replicate(100, new_location(x_loc, 5)))
+## x_loc <- c(3, 5)
+## new_locs_sd1 <- t(replicate(100, new_location(x_loc)))
+## new_locs_sd5 <- t(replicate(100, new_location(x_loc, 5)))
 
-plot(matrix(x_loc, ncol = 2), xlim = c(-20, 20), ylim = c(-20, 20), pch = "x")
-points(new_locs_sd1, col = "blue")
-points(new_locs_sd5, col = "red")
+## plot(matrix(x_loc, ncol = 2), xlim = c(-20, 20), ylim = c(-20, 20), pch = "x")
+## points(new_locs_sd1, col = "blue")
+## points(new_locs_sd5, col = "red")
--- R/quicksim.R
+++ R/quicksim.R
@@ -1,16 +1,23 @@
-## This script contains a series of functions which determine the state of a
-## newly infected case given its infector - e.g. geographic location, genetic
-## sequences, etc.
-
-
-## MAKE NEW INDIVIDUALS, EITHER FROM AN INFECTOR, OR FROM SCRATCH
+#' Create a new case
+#'
+#' This function creates a new case, with or without a known infector. If it is
+#' missing (NULL, default), the case is imported according to parameters in
+#' \code{config}.
+#'
+#' @export
+#'
+#' @param infector An optional infector; if provided, it must be a list with the
+#'     following slots: 'location', 'dna'
+#'
+#' @param ... further arguments passed to \code{new_config}
+#'
 
 new_case <- function(infector = NULL, ..., config = new_config(...)) {
     out <- list()
     
-    out$location <- new_location(infector, config = config)
+    out$location <- new_location(infector$location, config = config)
 
-    out$dna <- new_dna(infector, config = config)
+    out$dna <- new_dna(infector$dna, config = config)
 
     return(out)
 }
