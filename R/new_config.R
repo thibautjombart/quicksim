f46dfc6f00131feefb136977dc2542a857b97334
Thibaut Jombart <thibautjombart@gmail.com>
2018-04-06 11:24:15 +0100

    adding doc

--- R/new_config.R
+++ R/new_config.R
@@ -1,5 +1,35 @@
-
-
+#' Create a simulation configuration
+#'
+#' This function creates a list storing simulation settings - see details.
+#'
+#' @export
+#'
+#' @param ... named simulation settings - see details
+#'
+#' @details
+#'
+#' Simulation settings include:
+#' \itemize{
+#'   \item \code{x_min}: lower bound of spatial coordinates (x-axis)
+#'   \item \code{x_max}: upper bound of spatial coordinates (x-axis)
+#'   \item \code{y_min}: lower bound of spatial coordinates (y-axis)
+#'   \item \code{y_max}: upper bound of spatial coordinates (y-axis)
+#'   \item \code{sd_spatial}: standard deviation of the Normal kernel use in
+#'     spatial dispersion
+#'   \item \code{genome_length}: number of nucleotides in the pathogen genome
+#'   \item \code{mutation_rate}: per nucleotide and day
+#'   \item \code{separation_lineages}: number of days to ancestral lineage for
+#'     newly introduced pathogens 
+#' }
+#'
+#'
+#' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
+#'
+#' @examples
+#'
+#' new_config()
+#' new_config(genome_length = 100, xmax = 10)
+#' 
 new_config <- function(...) {
     defaults <- list(x_min = 0, # min x, spatial coords
                      x_max = 100, # max x, spatial coords
--- R/quicksim.R
+++ R/quicksim.R
@@ -4,16 +4,53 @@
 #' missing (NULL, default), the case is imported according to parameters in
 #' \code{config}.
 #'
+#' It calls upon \code{\link{new_location}} and \code{\link{new_dna}} to
+#' determine locations and DNA sequences.
+#'
 #' @export
 #'
 #' @param infector An optional infector; if provided, it must be a list with the
 #'     following slots: 'location', 'dna'
 #'
-#' @param ... further arguments passed to \code{new_config}
+#' @param date The date at which the new case is infected; if the case is
+#'   imported and this is not provided, defaults to 0.
 #'
+#' @param ... further arguments passed to \code{\link{new_config}}
+#'
+#' @param config optionally, a config returned by \code{\link{new_config}}
+#'
+#' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
+#'
+#' @examples
+#'
+#' ## imported cases
+#' A <- new_case()
+#' B <- new_case()
+#' A
+#' B
+#'
+#' ## transmission chain A -> C -> D
+#' C <- new_case(A, date = 3)
+#' D <- new_case(C, date = 10)
+#'
+#' A
+#' B
+#' C
+#' D
 
-new_case <- function(infector = NULL, ..., config = new_config(...)) {
+new_case <- function(infector = NULL, date = NULL,
+                     ..., config = new_config(...)) {
   out <- list()
+
+  if (is.null(date)) {
+    if (is.null(infector)) {
+      date <- 0L
+    } else {
+      stop("date of infection must be provided")
+    }
+  }
+
+  out$date <- date
   
   out$location <- new_location(infector$location, config = config)
   
